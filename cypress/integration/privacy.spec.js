1c9d1351c7d03664ee89b6b57b4b200ca62124a5
Kamila Figueredo <kfigqa@gmail.com>
2022-10-14 22:50:18 -0300

    Removing text capitalization form privacy test name

--- cypress/integration/privacy.spec.js
+++ cypress/integration/privacy.spec.js
@@ -1,6 +1,6 @@
 // Extra exercise: Running the same test case 'n' times using lodash (Cypress._).
 Cypress._.times(3, function() {
-  it('Independently tests the privacy policy page', function(){
+  it('independently tests the privacy policy page', function(){
     //Lesson 07: Extra exercise 2: Independently page tests
     cy.visit('./src/privacy.html')
   

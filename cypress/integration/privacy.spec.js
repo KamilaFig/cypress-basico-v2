1325731ac1f42286c403ca6bd295723c82f18ae5
Kamila Figueredo <kfigqa@gmail.com>
2022-10-14 22:00:17 -0300

    Using lodash to run the same test 'n' times

--- cypress/integration/privacy.spec.js
+++ cypress/integration/privacy.spec.js
@@ -1,7 +1,9 @@
-
-it.only('Independently tests the privacy policy page', function(){
-  //Lesson 07: Extra exercise 2: Independently page tests
-  cy.visit('./src/privacy.html')
-
-  cy.contains('Talking About Testing').should('be.visible')
+// Extra exercise: Running the same test case 'n' times using lodash (Cypress._).
+Cypress._.times(5, function() {
+  it('Independently tests the privacy policy page', function(){
+    //Lesson 07: Extra exercise 2: Independently page tests
+    cy.visit('./src/privacy.html')
+  
+    cy.contains('Talking About Testing').should('be.visible')
+  })
 })
